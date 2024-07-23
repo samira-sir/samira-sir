497c2560d9c7f1f946e1240a5d742aa80a6b3562
samira <snukanimnal@gmail.com>
2024-07-23 22:38:25 +0530

    Add files via upload

---
+++ js/rec.js
@@ -0,0 +1,21 @@
+const redirects = {
+    '6-history': 'recording/grade 6 history.html',
+    '6-geography': 'recording/grade 6 Geography.html',
+    '6-civic': 'recording/grade 6 Civic.html',
+    '7-history': 'recording/Grade 7 History.html',
+    '7-geography': 'recording/Grade 7 Geography.html',
+    '7-civic': 'recording/Grade 7 Civic.html',
+    '8-history': 'recording/Grade 8 History.html',
+    '8-geography': 'recording/Grade 8 Geography.html',
+    '8-civic': 'recording/Grade 8 Civic.html',
+    '9-history': 'recording/Grade 9 History.html',
+    '9-geography': 'recording/Grade 9 Geography.html',
+    '9-civic': 'recording/Grade 9 Civic.html',
+    '10-history': 'recording/grade 10 History.html',
+    '10-geography': 'recording/Grade 10 Geography.html',
+    '10-civic': 'recording/Grade 10 Civic.html',
+    '11-history': 'recording/Grade 11 History.html',
+    '11-geography': 'recording/Grade 11 Geography.html',
+    '11-civic': 'recording/Grade 11 Civic.html',
+ 
+  };
