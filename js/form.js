49baf6b169fe4105e864e1f955e94a5ba72a8395
sanuka-coder <snukanimnal@gmail.com>
2024-07-23 12:20:09 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,176 @@
+const urls = {
+    '6-history': [
+            'https://forms.gle/dqMfRjuTM96cR79z6',
+            'https://forms.gle/Tz1F22ypczRw37sx5',
+            'https://forms.gle/54aCqySvt59Pj3Ud8',
+            'https://forms.gle/HnKzaqLzoAjqkZB1A',
+            'https://forms.gle/FLrmuSATdtgAvdSu6',
+            'https://forms.gle/qnKD9e3j3oicFhRT9',
+            'https://forms.gle/7L1EE6sExt28XvuG6',
+            'https://forms.gle/W4jKcE4JikxAwfk79',
+            'https://forms.gle/cNy8GuUktrd3vQ789',
+            'https://forms.gle/qRBjQayhZ83gbgZZ8',
+            'https://forms.gle/xXYdj44gF9nG9gYV9',
+            
+          ],
+          '6-geography': [
+            'https://forms.gle/Y6wCEybEai1KaZhw9',
+            'https://forms.gle/J2va8KpzonWBE7j69',
+            'https://forms.gle/ieJxFuaeYChvv1RH7',
+            'https://forms.gle/HrQAtK6QtPX1V4687',
+            'https://forms.gle/L5Bksw8dqYeWPmob6',
+            'https://forms.gle/iuUSuqjuiLReaDzB7',
+            'https://forms.gle/FS22rKScL8XSAB459',
+            'https://forms.gle/DU6CvrJmtZRMeTzNA',
+            'https://forms.gle/9d1EWVxzRKuuENd38',
+            'https://forms.gle/oCgTCdKZ58Kznqmt9',
+            'https://forms.gle/Bor39y4SbbSq3YQ9A',
+            'https://forms.gle/jtK5pRRG1k4TvGhr7',
+            'https://forms.gle/hRVLSjpYAGAzdaak8',
+            'https://forms.gle/b6NraFigoDPfqGEg7',
+            'https://forms.gle/YnHAajHE5sgQhjeF6',
+            'https://forms.gle/kgCV8Ka5n6PuuwHp9',
+            'https://forms.gle/7ZhozamZR2Dz2Kmf9',
+            'https://forms.gle/JxeoDAEZHmz5ZbMy7',
+            'https://forms.gle/R8hQZzZdGXUpQgk39',
+            'https://forms.gle/2wjAcppjjBeyBjB86',
+            'https://forms.gle/irTMoEhd6uNkr3tS9',
+            'https://forms.gle/zByHt7e9GVLaCpJH9',
+            'https://forms.gle/jfz1nL9NbDAKDYug7',
+            'https://forms.gle/b738bYT6NViXakmu6'
+          ],
+          '6-civic': [
+            'https://forms.gle/EJEhVcmaaQwuJzjb6',
+            'https://forms.gle/SQ2Z9MwXgTMr6L4V6',
+            'https://forms.gle/FPWMoBiH9ij8kiAW6',
+            'https://forms.gle/3FpEAcMSLyC4o1ow6',
+            'https://forms.gle/by3fgaLjmfX1FBbu9',
+            'https://forms.gle/W8U6tcxRD6zurhJA7',
+            'https://forms.gle/EhjYofeTQGGCsC6P7',
+            'https://forms.gle/LA86tsbM2HqqtiR3A',
+            'https://forms.gle/s5vzCdD9vqnvsgBV7',
+            'https://forms.gle/knyzCzyi5x65Yc2t9',
+            'https://forms.gle/aqGi8ynzSw3b1hHh7',
+            'https://forms.gle/CNtRB4V3RHDnuHrZA',
+            'https://forms.gle/WYTTDRHooZJsp2aT9',
+            'https://forms.gle/xVFpFxAqWSxcBKpj8',
+            'https://forms.gle/3Wv8jmcAUATj136C7',
+            'https://forms.gle/8fzLVhM1JG2kaJoN7'
+            
+          ],
+          '7-history': [
+            'https://forms.gle/LjZeh7qScjvPczGCA',
+            'https://forms.gle/FCa4zhMdLCr1v1LUA',
+            'https://forms.gle/K5SzoBuZnbeZWoJW9',
+            'https://forms.gle/W3ATUxJyDZBwrRzC7',
+            'https://forms.gle/u3cDaeZhBMGKoLiy8',
+            'https://forms.gle/ZeuSJvQaLXSq4LR19',
+            'https://forms.gle/8oqkRJzWtFvgmsEx7',
+            'https://forms.gle/6GwMGM6ymfYvmRFo8',
+            'https://forms.gle/KyzGqyGXVJz7c7Uw6',
+            'https://forms.gle/vKUmpSqZqSh7LgAk8',
+            'https://forms.gle/XSh3DiyM8p6vojKFA',
+            'https://forms.gle/zZTdV6kT2ESd2Y5m6',
+            'https://forms.gle/JCnC21A4crAm6sDj7'
+          ],
+          '7-geography': [
+            'https://forms.gle/3SXPSS6kZpcYERtQ7',
+            'https://forms.gle/8Rvo9FD2q82aWMFPA',
+            'https://forms.gle/Nsj39acBk1BW5fZq6',
+            'https://forms.gle/Rd3MBXuLJycWNSk48',
+            'https://forms.gle/fsngAxwdt5VKBjV6A',
+            'https://forms.gle/ddY7Nz5EJ8XYTs6TA',
+            'https://forms.gle/9gxBJmCb19hvVnC86',
+            'https://forms.gle/hvNP9ushGazd7Pqw5',
+            'https://forms.gle/CGvp7wzNiRMYPQSE7',
+            'https://forms.gle/diEdtaH7ddJV3PMp7',
+            'https://forms.gle/tfkbnUur44xfjUM3A',
+            'https://forms.gle/AatsetnV2JZ3kwJE8',
+            'https://forms.gle/zYiiYhZNsCg54rho7',
+            'https://forms.gle/hVPtHyJmAK9HJur86',
+            'https://forms.gle/YixnqfvUcP6amPvw6',
+            
+          ],
+          '7-civic': [
+            '',
+            '',
+            'https://forms.gle/EWx2M48esoCybbQG7',
+            'https://forms.gle/fE8GqS47qXYQrpy76',
+            'https://forms.gle/KqfEVgajocEUXA4t8',
+            'https://forms.gle/R6zG4CpN7L5254NE6',
+            'https://forms.gle/w9rBZcQ3LjfKmkAt9',
+            'https://forms.gle/wrker7EoZsHfi4r69',
+            'https://forms.gle/3chZFwQ8t7iReSat5',
+            'https://forms.gle/MzUGZvbYKxZnPKzf7',
+            'https://forms.gle/mQsAvK9Kg6Ktfrv57',
+            'https://forms.gle/BPBCA3bakyjQWVkc6',
+            'https://forms.gle/EXAdHtMXyyJZpbW96',
+            'https://forms.gle/KAv8BiX9ow4b6pnT7',
+            'https://forms.gle/995JjaMagePQ8o1C7',
+            'https://forms.gle/UhCmRky43D4Auh6o8',
+            'https://forms.gle/CGhfTAX2o4ZySTGh9'
+            
+          ],
+          '8-history': [
+            'https://forms.gle/zeQNetJE7VFsRyV56',
+            'https://forms.gle/5BswrnfSdqna9Rh99',
+            'https://forms.gle/cA9QR3naz83uuREp6',
+            'https://forms.gle/w7z4fxFuCY6mj5WR8',
+            'https://forms.gle/jsoG5uTRmQ1iYaDt9',
+            'https://forms.gle/vBMWp7SRMZtzwkMC8',
+            'https://forms.gle/6eqapQ4K95Cb4W398',
+            'https://forms.gle/SBeubTqwLym4rYPFA',
+            'https://forms.gle/8PzHAXnLz2ywKdGQ9',
+            'https://forms.gle/LeL2xf4tX89fnSL6A',
+            'https://forms.gle/Y7KAH38jXquRGvU96'
+          ],
+          '8-geography': [
+            'https://forms.gle/U2yt6QW9ncp6cGRV9',
+            'https://forms.gle/1iYmfG7isELavpUr7',
+            'https://forms.gle/Ak7XXZeiiw7gxCCX7',
+            'https://forms.gle/mjUTauUHCBuZRWJU7',
+            'https://forms.gle/iKQRUYWUejygdV5n6',
+            'https://forms.gle/sjNJ1ovgy6ZLdVsb9',
+            'https://forms.gle/s3dX4qrmoxv194Nr9',
+            'https://forms.gle/GAFeNKh9ZCiGyuvD6'
+          ],
+          '8-civic': [
+            'https://forms.gle/ZB23X5kG87CBJPvLA',
+            'https://forms.gle/ipDKJPzotBJMzXyN8',
+            'https://forms.gle/mB7B7fccYKHYS7PD8',
+            'https://forms.gle/suy548CYNcyoeVgJ6',
+            'https://forms.gle/BTE5KukRw2mmECg86',
+            'https://forms.gle/BgyCQTMJ2xGGJATJ6',
+            'https://forms.gle/uwi4P3v5V87KGKUg8',
+            'https://forms.gle/ej7eUnnTM1JNenit6'
+          ],
+          '9-history': [
+            'https://forms.gle/D2CNSBojBEseVdjA6',
+  
+          ],
+          '9-geography': [
+            'https://forms.gle/MLUqPuMAqffNsK4p6',
+            'https://forms.gle/YPYRzvKbmW4Qt2q68'
+          ],
+          '9-civic': [
+            'https://forms.gle/5uvskKRdz5BryCX89'
+          ],
+          '10-history': [
+            'https://forms.gle/jM551j8wik78g2p69'
+          ],
+          '10-geography': [
+         
+          ],
+          '10-civic': [
+           
+          ],
+          '11-history': [
+         
+          ],
+          '11-geography': [
+            
+          ],
+          '11-civic': [
+            
+          ]
+      };
---
+++ js/wordwall.js
@@ -0,0 +1,61 @@
+const wallFormsUrls = {
+    '6-history': [
+      'https://wordwall.net/resource/34387148',
+      'https://wordwall.net/resource/34796947',
+      'https://wordwall.net/resource/75592606',
+
+    ],
+    '6-geography': [
+      'https://wordwall.net/resource/25526650',
+      'https://wordwall.net/resource/25525974',
+      'https://wordwall.net/resource/25632226',
+      'https://wordwall.net/resource/75592606',
+      
+    ],
+
+    '7-history': [
+      'https://wordwall.net/resource/34828617',
+      'https://wordwall.net/resource/66307689',
+      'https://wordwall.net/resource/75592606',
+
+    ],
+    '7-geography': [
+      'https://wordwall.net/resource/25631249',
+      'https://wordwall.net/resource/75593533',
+      'https://wordwall.net/resource/75592606',
+
+    ],
+    '9-history': [
+      'https://wordwall.net/resource/66309111',
+      'https://wordwall.net/resource/75592606',
+    ],
+    '9-geography': [
+      'https://wordwall.net/resource/75593257',
+      'https://wordwall.net/resource/75593055',
+      'https://wordwall.net/resource/75592606',
+    ],
+    '8-history': [
+
+      'https://wordwall.net/resource/75592606',
+    ],
+    '8-geography': [
+
+      'https://wordwall.net/resource/75592606',
+    ],
+    '10-history': [
+
+      'https://wordwall.net/resource/75592606',
+    ],
+    '10-geography': [
+
+      'https://wordwall.net/resource/75592606',
+    ],
+    '11-history': [
+      
+      'https://wordwall.net/resource/75592606',
+    ],
+    '11-geography': [
+     
+      'https://wordwall.net/resource/75592606',
+    ],
+  };
