f7b0afb540e5dbd9c2f9daeb232bae0b08b33cb7
samira <snukanimnal@gmail.com>
2024-07-25 21:51:12 +0530

    Update script.js

--- recording/script.js
+++ recording/script.js
@@ -4,3 +4,21 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = "../index.html";
     }
 });
+document.addEventListener('fullscreenchange', handleFullScreenChange);
+        document.addEventListener('webkitfullscreenchange', handleFullScreenChange);
+        document.addEventListener('mozfullscreenchange', handleFullScreenChange);
+        document.addEventListener('MSFullscreenChange', handleFullScreenChange);
+
+        function handleFullScreenChange() {
+            const videoContainer = document.getElementById('video-container');
+            const greeting = document.getElementById('greeting');
+
+            if (document.fullscreenElement === videoContainer ||
+                document.webkitFullscreenElement === videoContainer ||
+                document.mozFullScreenElement === videoContainer ||
+                document.msFullscreenElement === videoContainer) {
+                greeting.style.display = 'block';
+            } else {
+                greeting.style.display = 'none';
+            }
+        }
