5351cb7301c52f1fbbe5d35539c6591f969de697
samira <snukanimnal@gmail.com>
2024-07-27 18:20:41 +0530

    Update user.js

--- js/user.js
+++ js/user.js
@@ -4,7 +4,7 @@ const credentials = {
     numaya: { password: 'Pq3&Zs8#', subjects: [{ name: 'history', grade: 6 },{ name: 'civic', grade: 6 }] },
     kavitha: { password: 'Yx2$Ns9@', subjects: [{ name: 'geography', grade: 6 }, { name: 'history', grade: 6 },{ name: 'civic', grade: 6 }] },
     imandi: { password: 'Gw7@Eh5%', subjects: [{ name: 'history', grade: 6 }, { name: 'geography', grade: 6 },{ name: 'civic', grade: 6 }] },
-    danula: { password: 'Ju4#Hr6@', subjects: [{ name: 'history', grade: 6 }, { name: 'geography', grade: 6 },{ name: 'civic', grade: 6 }] },
+    dhanula: { password: 'Ju4#Hr6@', subjects: [{ name: 'history', grade: 6 }, { name: 'geography', grade: 6 },{ name: 'civic', grade: 6 }] },
     oneli: { password: 'Xc1&Ut9$', subjects: [{ name: 'history', grade: 6 },{ name: 'civic', grade: 6 }] },
     eesara: { password: 'Rm3$Jv7@', subjects: [{ name: 'geography', grade: 6 },{ name: 'history', grade: 6 },{ name: 'civic', grade: 6 }] },
     olindiya: { password: 'Po8%Lk2#', subjects: [{ name: 'history', grade: 6 },{ name: 'civic', grade: 6 }] },
