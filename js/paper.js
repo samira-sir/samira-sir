38a75055ec696260f1245e8ddd1852b6d17645ee
samira <snukanimnal@gmail.com>
2024-08-03 17:17:04 +0530

    Add files via upload

---
+++ js/paper.js
@@ -0,0 +1,43 @@
+
+const papersData = {
+    'sinhala': {
+        '6': {
+            'Science': {
+                '1st term': [
+                    { name: 'Science Paper 1A', link: 'https://drive.google.com/your-drive-link/sinhala/6/Science/1stTerm/Paper1A' },
+                    { name: 'Science Paper 1B', link: 'https://drive.google.com/your-drive-link/sinhala/6/Science/1stTerm/Paper1B' }
+                ],
+                '2nd term': [
+                    { name: 'Science Paper 2A', link: 'https://drive.google.com/your-drive-link/sinhala/6/Science/2ndTerm/Paper2A' },
+                    { name: 'Science Paper 2B', link: 'https://drive.google.com/your-drive-link/sinhala/6/Science/2ndTerm/Paper2B' }
+                ],
+                '3rd term': [
+                    { name: 'Science Paper 3A', link: 'https://drive.google.com/your-drive-link/sinhala/6/Science/3rdTerm/Paper3A' },
+                    { name: 'Science Paper 3B', link: 'https://drive.google.com/your-drive-link/sinhala/6/Science/3rdTerm/Paper3B' }
+                ]
+            },
+            // Add more subjects with their term papers for Grade 6 in Sinhala
+        },
+        // Add similar structure for grades 7-11 in Sinhala
+    },
+    'english': {
+        '6': {
+            'Science': {
+                '1st term': [
+                    { name: 'Science Paper 1A', link: 'https://drive.google.com/your-drive-link/english/6/Science/1stTerm/Paper1A' },
+                    { name: 'Science Paper 1B', link: 'https://drive.google.com/your-drive-link/english/6/Science/1stTerm/Paper1B' }
+                ],
+                '2nd term': [
+                    { name: 'Science Paper 2A', link: 'https://drive.google.com/your-drive-link/english/6/Science/2ndTerm/Paper2A' },
+                    { name: 'Science Paper 2B', link: 'https://drive.google.com/your-drive-link/english/6/Science/2ndTerm/Paper2B' }
+                ],
+                '3rd term': [
+                    { name: 'Science Paper 3A', link: 'https://drive.google.com/your-drive-link/english/6/Science/3rdTerm/Paper3A' },
+                    { name: 'Science Paper 3B', link: 'https://drive.google.com/your-drive-link/english/6/Science/3rdTerm/Paper3B' }
+                ]
+            },
+            // Add more subjects with their term papers for Grade 6 in English
+        },
+        // Add similar structure for grades 7-11 in English
+    }
+};
