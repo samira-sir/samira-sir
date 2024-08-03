da6f93bf51dbc5f0cfb5d05b0420a86014ccf20d
samira <snukanimnal@gmail.com>
2024-08-03 12:56:43 +0530

    Update pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -1,1 +1,11 @@
-
+const pdfUrls = {
+    "6-history": [
+      "pdfs/6-history-1.pdf",
+      "pdfs/6-history-2.pdf",
+      "pdfs/6-history-3.pdf",
+      "pdfs/6-history-4.pdf",
+      "pdfs/6-history-5.pdf",
+      "pdfs/6-history-6.pdf"
+    ],
+    // Add other class-subject combinations here
+  };
