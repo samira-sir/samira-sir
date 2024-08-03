e501155efdc34791ecf6c798f0b4ab4d2cb13052
samira <snukanimnal@gmail.com>
2024-08-03 13:09:09 +0530

    Update pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -1,11 +1,31 @@
 const pdfUrls = {
     "6-history": [
-      "pdfs/6-history-1.pdf",
-      "pdfs/6-history-2.pdf",
-      "pdfs/6-history-3.pdf",
-      "pdfs/6-history-4.pdf",
-      "pdfs/6-history-5.pdf",
-      "pdfs/6-history-6.pdf"
+      "https://drive.google.com/file/d/1_CVezGJ_s0xN0Ag444bd6XtYERVdn5ak/view?usp=sharing",
+      "https://drive.google.com/file/d/1awrxFXvTE81nCj-l6gMKZaMcw5x3ZmXz/view?usp=sharing",
     ],
-    // Add other class-subject combinations here
+     "6-civic": [
+
+    ],
+    "6-geography": [
+      "https://drive.google.com/file/d/1cR--K3p9Xr-Po_aQLIw7W-iFx2wYpKd9/view?usp=sharing",
+      "https://drive.google.com/file/d/1LtXqy9SvzpZjEgvJWx3oG7supfVxtzpb/view?usp=sharing",
+    ],
+ "7-history": [
+      "https://drive.google.com/file/d/11_E_Mlq1PgM3no9n40RreYBu3cDBmFKE/view?usp=sharing",
+      "https://drive.google.com/file/d/14gkq9yA-zRYrfMJmO-r6cdVEcK1mlefU/view?usp=sharing",
+     "https://drive.google.com/file/d/13urskEr90a7iv3HajZwaNoNV8rruWisd/view?usp=sharing",
+     "https://drive.google.com/file/d/1ERN_IqBDegGe9VbcLMA4kcmT2cJ3Z1Df/view?usp=sharing",
+     "https://drive.google.com/file/d/1TXw9z-vKwesM2TiAGqP06cqbux_Feoo_/view?usp=sharing",
+     "https://drive.google.com/file/d/1QDSvrnAb6utzq3t5dV8lr_GWHTtOBill/view?usp=sharing",
+     
+    ],
+ "8-history": [
+      "https://drive.google.com/file/d/11_E_Mlq1PgM3no9n40RreYBu3cDBmFKE/view?usp=sharing",
+      "https://drive.google.com/file/d/14gkq9yA-zRYrfMJmO-r6cdVEcK1mlefU/view?usp=sharing",
+     "https://drive.google.com/file/d/13urskEr90a7iv3HajZwaNoNV8rruWisd/view?usp=sharing",
+     "https://drive.google.com/file/d/1ERN_IqBDegGe9VbcLMA4kcmT2cJ3Z1Df/view?usp=sharing",
+     "https://drive.google.com/file/d/1TXw9z-vKwesM2TiAGqP06cqbux_Feoo_/view?usp=sharing",
+     "https://drive.google.com/file/d/1QDSvrnAb6utzq3t5dV8lr_GWHTtOBill/view?usp=sharing",
+     
+    ],
   };
