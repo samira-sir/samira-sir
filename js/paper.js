9e12ea0a51029eccedddff8f45fad100b47bf75d
samira <snukanimnal@gmail.com>
2024-08-08 20:33:35 +0530

    Update paper.js

--- js/paper.js
+++ js/paper.js
@@ -1,4 +1,3 @@
-
 const papersData = {
     'sinhala': {
         '6': {
@@ -15,60 +14,40 @@ const papersData = {
                     { name: 'Science Paper 3A', link: '' },
                     { name: 'Science Paper 3B', link: '' }
                 ]
-            },
+            }
             // Add more subjects with their term papers for Grade 6 in Sinhala
         },
-         '9': {
+        '9': {
             'Mathematics': {
-                '1st term': [
-
-                ],
+                '1st term': [],
                 '2nd term': [
                     { name: '09 ශ්‍රේණිය දෙවන වාරය ගණිතය ප්‍රශ්න පත්‍ර එකතුව', link: 'https://drive.google.com/file/d/1XYNQekoBSaJKt4Rk7y-wJh0t6xk5qR0N/view?usp=drive_link' },
                 ],
-                '3rd term': [
-
-             ]
-            },
-            // Add more subjects with their term papers for Grade 6 in Sinhala
+                '3rd term': []
+            }
+            // Add more subjects with their term papers for Grade 9 in Sinhala
         },
-        // Add similar structure for grades 7-11 in Sinhala
-     '11': {
-            'Buddhism': {
-                '1st term': [
-
-                ],
-                '2nd term': [
-                    
-                ],
-                '3rd term': [
-                    { name: 'BUDDHISM-OL-Note-Tute-2024', link: 'https://drive.google.com/file/d/1VoSjsqRonmeF4BLFJUnnWNBSUjFaqDcv/view?usp=sharing' },
-             ]
-            },
-            // Add more subjects with their term papers for Grade 6 in Sinhala
-        },
-
-    '10': {
+        '10': {
             'History': {
-                '1st term': [
-
-                ],
+                '1st term': [],
                 '2nd term': [
-                     { name: 'Southern-Province-Grade-10-History-Second-Term-Paper-2021-–-Sinhala-Medium', link: 'https://drive.google.com/file/d/1XxW0Uf9lxTitjEtqmb-2U9L1QX5ma032/view?usp=drive_link' },
-             ]
+                    { name: 'Southern-Province-Grade-10-History-Second-Term-Paper-2021-–-Sinhala-Medium', link: 'https://drive.google.com/file/d/1XxW0Uf9lxTitjEtqmb-2U9L1QX5ma032/view?usp=drive_link' }
                 ],
-                '3rd term': [
-                   
-            },
-            // Add more subjects with their term papers for Grade 6 in Sinhala
+                '3rd term': []
+            }
+            // Add more subjects with their term papers for Grade 10 in Sinhala
         },
+        '11': {
+            'Buddhism': {
+                '1st term': [],
+                '2nd term': [],
+                '3rd term': [
+                    { name: 'BUDDHISM-OL-Note-Tute-2024', link: 'https://drive.google.com/file/d/1VoSjsqRonmeF4BLFJUnnWNBSUjFaqDcv/view?usp=sharing' }
+                ]
+            }
+            // Add more subjects with their term papers for Grade 11 in Sinhala
+        }
     },
-
-
-
-
-
-    
     'english': {
         '6': {
             'Science': {
@@ -84,69 +63,9 @@ const papersData = {
                     { name: 'Science Paper 3A', link: 'https://drive.google.com/your-drive-link/english/6/Science/3rdTerm/Paper3A' },
                     { name: 'Science Paper 3B', link: 'https://drive.google.com/your-drive-link/english/6/Science/3rdTerm/Paper3B' }
                 ]
-            },
+            }
             // Add more subjects with their term papers for Grade 6 in English
         },
         // Add similar structure for grades 7-11 in English
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
