074d1fbf9c1b507ee26299d28cb1df62fdd1ec12
samira <snukanimnal@gmail.com>
2024-08-10 17:40:19 +0530

    Update user.js

--- js/user.js
+++ js/user.js
@@ -72,7 +72,7 @@ const credentials = {
     thisumi: { password: 'Wu8%Kr3@', subjects: [{ name: 'history', grade: 9 },{ name: 'geography', grade: 9 },{ name: 'civic', grade: 9},{ name: 'ind-Thisumi-History', grade: 9 }] },
     nishad: { password: 'Gt4&Pe9%', subjects: [{ name: 'geography', grade: 9 },{ name: 'civic', grade: 9}] },
     dihen: { password: 'D@i$h#e9N,', subjects: [{ name: 'history', grade: 10 },{ name: 'geography', grade: 10 }] },
-    hesaka: { password: 'heasaka2009', subjects: [{ name: 'history', grade: 10 }] },
+    hesaka: { password: 'hesaka2009', subjects: [{ name: 'history', grade: 10 }] },
     sithnuka: { password: 'Gp3%Fn8@', subjects: [{ name: 'history', grade: 10 }] },
     madumi: { password: 'Ho7&Kw2%', subjects: [{ name: 'history', grade: 10 },{ name: 'geography', grade: 10 }] },
     pasandi: { password: 'Tr4@Qs9&', subjects: [{ name: 'history', grade: 10 },{ name: 'geography', grade: 10 }] },
