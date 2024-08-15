bfff2877c0a19a566821525a735332c9c16f0b36
samira <snukanimnal@gmail.com>
2024-08-15 21:52:40 +0530

    Update wordwall.js

--- js/wordwall.js
+++ js/wordwall.js
@@ -3,6 +3,7 @@ const wallFormsUrls = {
       'https://wordwall.net/resource/34387148',
       'https://wordwall.net/resource/34796947',
       'https://wordwall.net/resource/75592606',
+        'https://www.educaplay.com/learning-resources/19946453-grade_6_history_samira_sir.html',
 
     ],
     '6-geography': [
@@ -10,6 +11,10 @@ const wallFormsUrls = {
       'https://wordwall.net/resource/25525974',
       'https://wordwall.net/resource/25632226',
       'https://wordwall.net/resource/75592606',
+        'https://www.educaplay.com/learning-resources/19946374-countries_in_the_world_by_samira_ariyadasa.html',
+        'https://www.educaplay.com/learning-resources/19945434-grade_7_geography_samira_sir.html',
+        'https://www.educaplay.com/learning-resources/19946117-elemental_quest.html',
+        
       
     ],
 
@@ -23,25 +28,34 @@ const wallFormsUrls = {
       'https://wordwall.net/resource/25631249',
       'https://wordwall.net/resource/75593533',
       'https://wordwall.net/resource/75592606',
+        'https://www.educaplay.com/learning-resources/19946374-countries_in_the_world_by_samira_ariyadasa.html',
+        'https://www.educaplay.com/learning-resources/19946117-elemental_quest.html',
 
     ],
     '9-history': [
       'https://wordwall.net/resource/66309111',
       'https://wordwall.net/resource/75592606',
+        'https://www.educaplay.com/learning-resources/19945915-grade_9_history_samira_sir.html',
     ],
     '9-geography': [
       'https://wordwall.net/resource/75593257',
       'https://wordwall.net/resource/75593055',
       'https://wordwall.net/resource/75592606',
+        'https://www.educaplay.com/learning-resources/19946374-countries_in_the_world_by_samira_ariyadasa.html',
+        'https://www.educaplay.com/learning-resources/19946117-elemental_quest.html',
+        'https://www.educaplay.com/learning-resources/19945352-yes_or_n_geography_grade_9.html',
     ],
     '8-history': [
 
       'https://wordwall.net/resource/75592606',
+     'https://wordwall.net/resource/76612518',
     ],
     '8-geography': [
 
       'https://wordwall.net/resource/75592606',
      'https://www.educaplay.com/learning-resources/19946117-elemental_quest.html',
+        'https://www.educaplay.com/learning-resources/19946374-countries_in_the_world_by_samira_ariyadasa.html',
+        'https://www.educaplay.com/learning-resources/19946117-elemental_quest.html',
     ],
     '10-history': [
 
@@ -50,6 +64,9 @@ const wallFormsUrls = {
     '10-geography': [
 
       'https://wordwall.net/resource/75592606',
+        'https://www.educaplay.com/learning-resources/19946374-countries_in_the_world_by_samira_ariyadasa.html',
+        'https://www.educaplay.com/learning-resources/19946117-elemental_quest.html',
+        
     ],
     '11-history': [
       
