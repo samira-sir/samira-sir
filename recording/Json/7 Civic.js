c25c455ae4779027ceb5fdb7ef88fb2f85957ff6
samira <snukanimnal@gmail.com>
2024-09-07 14:02:23 +0530

    Add files via upload

---
+++ recording/Json/7 Civic.js
@@ -0,0 +1,33 @@
+const videosList = [
+    { video: 'https://drive.google.com/file/d/16iKcPq_Z9g2qPQOLI94HE0uwQzNA1z29/preview', title: '2024-07-28 1' },
+    { video: 'https://drive.google.com/file/d/11LovnhXKQpzAdFMKqHiGJSo_Zcm1J8IB/preview', title: '2024-07-28 2' },
+    { video: 'https://drive.google.com/file/d/1Xu2If-2dxEboS8FnNauTsGAdOIKyvHXD/preview', title: '2024-07-28 3' },
+{ video: 'https://drive.google.com/file/d/1hTV34bBJBh4F0bKnJK1eeIXNpPnmMM67/preview', title: '2024-08-04 1' },
+    { video: 'https://drive.google.com/file/d/1SShStrZigsW_OZYcTjlNfNI52vYfPHLQ/preview', title: '2024-08-04 2' },
+    { video: 'https://drive.google.com/file/d/1jNWDSqd9t5jV-VFJ_OJQ-JoYOQCkAX9B/preview', title: '2024-08-11 1' },
+    { video: 'https://drive.google.com/file/d/1d0d5hXL-LTSTZB7yUcxoYe8Uk4Nzadkj/preview', title: '2024-08-11 2' },
+    { video: 'https://drive.google.com/file/d/1duMct2AlY0TmJwy2CV9v2jhiQLEGt5AD/preview', title: '2024-08-11 3' },
+{ video: 'https://drive.google.com/file/d/1vrlcUSz88RJsfoREqj8s1K4RYA2dWaz-/preview', title: '2024-08-18 1' },
+    { video: 'https://drive.google.com/file/d/1lJDbhF6EjHbjWCjC7pimGlvVy0QNTgWv/preview', title: '2024-08-18 2' },
+    { video: 'https://drive.google.com/file/d/1DsxiS7ca6NB3c8dosxiabUXDBtFJwJ6a/preview', title: '2024-08-18 3' },
+{ video: 'https://drive.google.com/file/d/1EBaGLe8x0shDGYqBIoBrc3p006T0fDu8/preview', title: '2024-08-24 1' },
+    { video: 'https://drive.google.com/file/d/1-mPkrMqBaKO8TH5vfb-LiWdEQ-KdLxiw/preview', title: '2024-08-24 2' },
+    { video: 'https://drive.google.com/file/d/1Oh4uS2FCId89ry6jT9fnnFQhOFozeJFn/preview', title: '2024-08-24 3' },
+{ video: 'https://drive.google.com/file/d/13zh7G6Yb2lkOxeZHEr3kJt1xXdS_Sv0H/preview', title: '2024-09-01 1' },
+    { video: 'https://drive.google.com/file/d/15IK-gBq0jtIo00QApYAMkZnXg-TGKKSf/preview', title: '2024-09-01 2' },
+    { video: 'https://drive.google.com/file/d/1ZFn0j2Rs4Yx8MrWyE4BeuL-14mvmndrK/preview', title: '2024-09-01 3' },
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+];
