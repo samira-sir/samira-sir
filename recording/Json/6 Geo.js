709d553696d094f82142f62130ecfdae097e45e7
samira <snukanimnal@gmail.com>
2024-09-08 21:14:27 +0530

    Update 6 Geo.js

--- recording/Json/6 Geo.js
+++ recording/Json/6 Geo.js
@@ -4,7 +4,11 @@ const videosList = [
     { video: 'https://drive.google.com/file/d/1T1Yu6-PB94FAsHA-AGkXQTtxakkn5NxE/preview', title: '2024 08 03 3' },
 { video: 'https://drive.google.com/file/d/1ddY85d5sdy8qsAuWFa1MVpl0JFXJnC4Y/preview', title: '2024 08 24 1' },
     { video: 'https://drive.google.com/file/d/1fvVkYGXKr05SPvZ5o2ZhZl52R1Djh1XQ/preview', title: '2024 08 24 2' },
-    { video: 'https://drive.google.com/file/d/1BBGD9VICJEu-uem5wRbOqYCg2KtdM2dn/preview', title: '2024 08 24 3' },	              
+    { video: 'https://drive.google.com/file/d/1BBGD9VICJEu-uem5wRbOqYCg2KtdM2dn/preview', title: '2024 08 24 3' },	   
+    { video: 'https://drive.google.com/file/d/1dilbDJe4qe_ipM2SMkxMpRk7gEzakJPn/preview', title: 'Grade 6 Geo 2024-09-07 Part 1' },
+{ video: 'https://drive.google.com/file/d/1XI6Jt8_28N4vH4OjlIuGhu2sT7__U60B/preview', title: 'Grade 6 Geo 2024-09-07 Part 2' },
+{ video: 'https://drive.google.com/file/d/1GVs2cH4vr6md_-x33ZtgpMn5jHEZlrzw/preview', title: 'Grade 6 Geo 2024-09-07 Part 3' },
+
 
 
 
