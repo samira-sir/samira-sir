7d40af8badcf66986032b25757a896780d4b7627
samira <snukanimnal@gmail.com>
2024-10-12 07:00:36 +0530

    Update 8 Civic.js

--- recording/Json/8 Civic.js
+++ recording/Json/8 Civic.js
@@ -1,8 +1,5 @@
 const videosList = [
-    { video: 'https://drive.google.com/file/d/1UzdKDjsB3WUeEoV1G9DjslgbahCUoaux/preview', title: '2024 08 02 02' },
-    { video: 'https://drive.google.com/file/d/100-mtLZFOmzB_ZPKtUoysybvOXEya36m/preview', title: '2024 08 02 03' },
-    { video: 'https://drive.google.com/file/d/1IE7aJ6SZjybAcI92kpMiPHHlt1sqhxIS/preview', title: '2024 08 09 01' },
-    { video: 'https://drive.google.com/file/d/1d_MvPBAthuLbxPFYLmVd1fguSG-X4XgN/preview', title: '2024 08 09 02' },
+     { video: 'https://drive.google.com/file/d/1d_MvPBAthuLbxPFYLmVd1fguSG-X4XgN/preview', title: '2024 08 09 02' },
     { video: 'https://drive.google.com/file/d/1VC-dqnJDRdI_epW6cmB4zjvJhBf3mJvI/preview', title: '2024 08 09 03' },
     { video: 'https://drive.google.com/file/d/1WvZVwY5A_1794rIVt5yYjXtj29FMNTH_/preview', title: '2024 08 16 01' },
 { video: 'https://drive.google.com/file/d/1O8APmp0neS_vS4nuqU7fBPZEaQvfCEnu/preview', title: '2024 08 16 02' },
@@ -22,6 +19,9 @@ const videosList = [
 { video: 'https://drive.google.com/file/d/10jV18b94EyLJv52o2y81weTirBeR8sIH/preview', title: 'Grade 8 Civic 2024-09-27 Part 2' },
 { video: 'https://drive.google.com/file/d/1k_YmV113GJwPRfJMkmARMrAIrS1J1b-o/preview', title: 'Grade 8 Civic 2024-10-04 Part 1' },
 { video: 'https://drive.google.com/file/d/1lAPC8biObaiNuIcPCCkAP2-C3wq6uTl4/preview', title: 'Grade 8 Civic 2024-10-04 Part 2' },
+{ video: 'https://drive.google.com/file/d/1gpdTxFPxTo8bH9doR0vlX4dn_izT0dE2/preview', title: 'Grade 8 Civic 2024-10-11 Part 1' },
+{ video: 'https://drive.google.com/file/d/1vjUnt1iTiEIv6A1hCCHAO00avKepyX72/preview', title: 'Grade 8 Civic 2024-10-11 Part 2' },
+{ video: 'https://drive.google.com/file/d/1eLSFy13rY-cI4_hndNYKoORD6CU7DvoJ/preview', title: 'Grade 8 Civic 2024-10-11 Part 3' },
 
 
 
