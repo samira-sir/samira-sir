21c7d2e2bec80b67f28da5709d5d4dec5d5bc58c
samira <snukanimnal@gmail.com>
2024-10-13 22:03:11 +0530

    Update 6 History.js

--- recording/Json/6 History.js
+++ recording/Json/6 History.js
@@ -1,8 +1,4 @@
 const videosList = [
-    { video: 'https://drive.google.com/file/d/1bEh_IDO5R8tyr-DUCRv0EK-_rw3ggec-/preview', title: '2024-07-27 01' },
-    { video: 'https://drive.google.com/file/d/1focUf6v9u6Cq0Emr4mwR1-xq6ri4CYp2/preview', title: '2024-07-27 02' },
-    { video: 'https://drive.google.com/file/d/1Kj9E8ZOGGV4cogahhOfcDHsMFnIML8ez/preview', title: '2024-07-27 03' },
-    { video: 'https://drive.google.com/file/d/1UUha5JYXio5IDZKO5lgBY83Vxb7SRyed/preview', title: '2024-07-27 04' },
     { video: 'https://drive.google.com/file/d/1h5FPW71sU7eX981X6aVGn6GhiJqUpoaC/preview', title: '2024-08-03 01' },
     { video: 'https://drive.google.com/file/d/1_s6FiLvuKELlDSEc6uk50JJNvHqeeKnU/preview', title: '2024-08-03 02' },
     { video: 'https://drive.google.com/file/d/1K9eSstQNCuZhpEulccQtj6CHGJUJZeRE/preview', title: '2024-08-03 03' },
@@ -23,6 +19,9 @@ const videosList = [
 { video: 'https://drive.google.com/file/d/1EFwTLJ0-HK2JzsWsY9_Gc42xJpJMYlZB/preview', title: 'Grade 6 History 2024-10-05 Part 2' },
 { video: 'https://drive.google.com/file/d/1Koxh86oJ90f7Zpyr0Nanuz08akvvkqFW/preview', title: 'Grade 6 History 2024-10-05 Part 3' },
 { video: 'https://drive.google.com/file/d/1igTWH4eGpipJ3iNehGhViNy2FNc9mbZf/preview', title: 'Grade 6 History 2024-10-05 Part 4' },
+{ video: 'https://drive.google.com/file/d/14CVD9xAmOfRaBqByrGcyMQWTTgZZiCaZ/preview', title: 'Grade 6 History 2024-10-12 Part 1' },
+{ video: 'https://drive.google.com/file/d/10bW4pUUA3itQS0MRZeuIOFC2l3U2mnxi/preview', title: 'Grade 6 History 2024-10-12 Part 2' },
+{ video: 'https://drive.google.com/file/d/1Oyph09WmUzeEa-nQuCWrNp-l0xSbD6lw/preview', title: 'Grade 6 History 2024-10-12 Part 3' },
 
 
 
