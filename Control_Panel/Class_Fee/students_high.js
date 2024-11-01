75be3b0a19489b2739eaa740a4e699719f40ffff
samira <snukanimnal@gmail.com>
2024-11-01 12:59:08 +0530

    Add files via upload

---
+++ Control_Panel/Class_Fee/students_high.js
@@ -0,0 +1,69 @@
+const students = [
+    {
+        name: "Dihen",
+        classes: ["10"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Hesaka",
+        classes: ["10"],
+        subjects: ["History", ]
+    },
+    {
+        name: "Sithnuka",
+        classes: ["10"],
+        subjects: ["History"]
+    },
+    {
+        name: "Madumi",
+        classes: ["10"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Pasandi",
+        classes: ["10"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Dulya",
+        classes: ["10"],
+        subjects: ["History"]
+    },
+    {
+        name: "Pahanmi",
+        classes: ["10"],
+        subjects: ["Geography"]
+    },
+    {
+        name: "Sanul",
+        classes: ["10"],
+        subjects: ["Geography"]
+    },
+    {
+        name: "Nethini",
+        classes: ["10"],
+        subjects: ["Geography"]
+    },
+    {
+        name: "Ometh",
+        classes: ["10"],
+        subjects: ["Geography"]
+    },
+    {
+        name: "Thinuli",
+        classes: ["10"],
+        subjects: ["Geography","History"]
+    },
+    {
+        name: "Malishi",
+        classes: ["10"],
+        subjects: ["Geography"]
+    },
+    {
+        name: "Dulani",
+        classes: ["10"],
+        subjects: ["History"]
+    }
+];
+
+// You can continue adding more students with their respective classes and subjects.
---
+++ Control_Panel/Class_Fee/students_low.js
@@ -0,0 +1,312 @@
+const students = [
+
+    {
+        name: "Numaya",
+        classes: ["6"],
+        subjects: ["History"]
+    },
+    {
+        name: "Kavitha",
+        classes: ["6"],
+        subjects: ["Geography", "History"]
+    },
+    {
+        name: "Imandi",
+        classes: ["6"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Danula",
+        classes: ["6"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Oneli",
+        classes: ["6"],
+        subjects: ["History"]
+    },
+    {
+        name: "Eesara",
+        classes: ["6"],
+        subjects: ["History"]
+    },
+    {
+        name: "Olindiya",
+        classes: ["6"],
+        subjects: ["History"]
+    },
+    {
+        name: "Dulansa",
+        classes: ["6"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Chenuka",
+        classes: ["6"],
+        subjects: ["Geography", "History"]
+    },
+    {
+        name: "Dinura",
+        classes: ["7"],
+        subjects: ["History", "Civic", "Geography"]
+    },
+    {
+        name: "Pranami",
+        classes: ["7"],
+        subjects: ["Geography"]
+    },
+    {
+        name: "Ushana",
+        classes: ["7"],
+        subjects: ["Geography"]
+    },
+    {
+        name: "Vonadi",
+        classes: ["7"],
+        subjects: ["History"]
+    },
+    {
+        name: "Dulnitha",
+        classes: ["7"],
+        subjects: ["History"]
+    },
+    {
+        name: "Januni",
+        classes: ["7"],
+        subjects: ["History"]
+    },
+    {
+        name: "Radheesha",
+        classes: ["7"],
+        subjects: ["History"]
+    },
+    {
+        name: "Chamuditha",
+        classes: ["7"],
+        subjects: ["Geography", "History", "Civic"]
+    },
+    {
+        name: "Sansala",
+        classes: ["7"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Sadew",
+        classes: ["7"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Samindya",
+        classes: ["7"],
+        subjects: ["Geography", "History"]
+    },
+    {
+        name: "Kulsum",
+        classes: ["7"],
+        subjects: ["Geography", "History", "Civic"]
+    },
+    {
+        name: "Chenul",
+        classes: ["7"],
+        subjects: ["Geography", "History"]
+    },
+    {
+        name: "Sandeepa",
+        classes: ["7"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Anneq",
+        classes: ["7"],
+        subjects: ["History"]
+    },
+    {
+        name: "Navodya",
+        classes: ["7"],
+        subjects: ["History"]
+    },
+    {
+        name: "Oshadi",
+        classes: ["7"],
+        subjects: ["History"]
+    },
+    {
+        name: "Nethuki",
+        classes: ["7"],
+        subjects: ["Geography"]
+    },
+    {
+        name: "Senuli",
+        classes: ["7"],
+        subjects: ["Geography"]
+    },
+    {
+        name: "Senuka",
+        classes: ["8"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Sadil",
+        classes: ["8"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Kethmin",
+        classes: ["8"],
+        subjects: ["History", "Geography",]
+    },
+    {
+        name: "Damindu",
+        classes: ["8"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Ruzni",
+        classes: ["8"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Eshini",
+        classes: ["8"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Nishini",
+        classes: ["8"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Yahansa",
+        classes: ["8"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Kavithma",
+        classes: ["8"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Reema",
+        classes: ["8"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Mihin",
+        classes: ["8"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Thinuki",
+        classes: ["8"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Lakni",
+        classes: ["8"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Ahansa",
+        classes: ["8"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Senudi",
+        classes: ["8"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Manuka",
+        classes: ["8"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Eeshara",
+        classes: ["8"],
+        subjects: ["History"]
+    },
+    {
+        name: "Lithuli",
+        classes: ["8"],
+        subjects: ["History"]
+    },
+    {
+        name: "Lahiru",
+        classes: ["8"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Senulya",
+        classes: ["8"],
+        subjects: ["History"]
+    },
+    {
+        name: "Minketh",
+        classes: ["8"],
+        subjects: ["Geography", "Civic"]
+    },
+    {
+        name: "Ishan",
+        classes: ["9"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Udula",
+        classes: ["9"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Praveen",
+        classes: ["9"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Silma",
+        classes: ["9"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Hashini",
+        classes: ["9"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Kavini",
+        classes: ["9"],
+        subjects: ["History"]
+    },
+    {
+        name: "Santhuli",
+        classes: ["9"],
+        subjects: ["History"]
+    },
+    {
+        name: "Senula",
+        classes: ["9"],
+        subjects: ["History", "Geography", "Civic"]
+    },
+    {
+        name: "Methuki",
+        classes: ["9"],
+        subjects: ["History"]
+    },
+    {
+        name: "Saniru",
+        classes: ["9"],
+        subjects: ["History", "Geography"]
+    },
+    {
+        name: "Thisumi",
+        classes: ["9"],
+        subjects: [ "Geography", "Civic"]
+    },
+    {
+        name: "Nishad",
+        classes: ["9"],
+        subjects: ["Geography", "Civic"]
+    },
+
+
+];
+
+// You can continue adding more students with their respective classes and subjects.
