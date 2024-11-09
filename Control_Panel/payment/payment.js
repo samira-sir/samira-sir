b3b2acfe37bbcf6bfe2e2dd9d424cb11f97ebcbd
samira <snukanimnal@gmail.com>
2024-11-09 12:18:58 +0530

    Add files via upload

---
+++ Control_Panel/payment/payment.js
@@ -0,0 +1,19 @@
+// payment/payment.js
+const billArray = [
+    {"name":"sanuka","grade":"9","subjects":["geography","civic","history","ind-Thisumi-History","ind-Disura-History"],"month":"December","totalFee":3500,"referenceNumber":"REF927513","dateTime":"11/9/2024, 11:12:49 AM"},
+    {"name":"samira","grade":"10","subjects":["history","geography","civic"],"month":"November","totalFee":2400,"referenceNumber":"REF920642","dateTime":"11/9/2024, 11:09:41 AM"},
+    {
+        "name": "pranami",
+        "grade": "7",
+        "subjects": [
+          "geography",
+          "history"
+        ],
+        "month": "November",
+        "totalFee": 1400,
+        "referenceNumber": "REF253913",
+        "dateTime": "2024-11-09T06:10:47.466Z"
+      },
+];
+
+// Export the array for use in other files
