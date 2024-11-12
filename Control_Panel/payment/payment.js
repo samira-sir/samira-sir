dcf20954b2dc4bcfd8702f8a1af4fa35936eb0bc
samira <snukanimnal@gmail.com>
2024-11-12 08:14:54 +0530

    Update payment.js

--- Control_Panel/payment/payment.js
+++ Control_Panel/payment/payment.js
@@ -13,6 +13,35 @@ const billArray = [
         "referenceNumber": "REF253913",
         "dateTime": "2024-11-09T06:10:47.466Z"
       },
+
+{
+  "name": "dulansa",
+  "grade": "6",
+  "subjects": [
+    "history",
+    "geography"
+  ],
+  "month": "October",
+  "totalFee": 1400,
+  "referenceNumber": "REF103615",
+  "dateTime": "2024-11-12T02:43:39.920Z"
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 ];
 
 // Export the array for use in other files
