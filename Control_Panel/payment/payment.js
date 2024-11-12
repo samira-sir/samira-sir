6c0de375bf22100aad5051ff1fa3a0d32e9b6e83
samira <snukanimnal@gmail.com>
2024-11-12 15:02:22 +0530

    Update payment.js

--- Control_Panel/payment/payment.js
+++ Control_Panel/payment/payment.js
@@ -25,6 +25,23 @@ const billArray = [
   "totalFee": 1400,
   "referenceNumber": "REF103615",
   "dateTime": "2024-11-12T02:43:39.920Z"
+
+{
+  "name": "osadee",
+  "grade": "7",
+  "subjects": [
+    "history"
+  ],
+  "month": "November",
+  "totalFee": 700,
+  "referenceNumber": "REF518177",
+  "dateTime": "2024-11-12T09:28:08.814Z"
+},
+
+
+
+
+    
 },
 
 
