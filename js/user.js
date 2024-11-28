064ffb7ffa769cac23b169170a9a24151e3ef9f3
samira <snukanimnal@gmail.com>
2024-11-28 10:42:46 +0530

    Update user.js

--- js/user.js
+++ js/user.js
@@ -24,7 +24,7 @@ const credentials = {
     kulsum: { password: 'MKM2812', subjects: [{ name: 'geography', grade: 7 },{ name: 'history', grade: 7 },{ name: 'civic', grade: 7 }] },
     chenul: { password: 'Me7%Pq3@', subjects: [{ name: 'geography', grade: 7 },{ name: 'history', grade: 7 }] },        
     sandeepa: { password: 'Kf5@Hj2&', subjects: [{ name: 'history', grade: 7 },{ name: 'geography', grade: 7 }] },
-    anneq: { password: 'Ir1&Lw9%', subjects: [{ name: 'history', grade: 7 },] },
+    anneq: { password: 'anneq123', subjects: [{ name: 'history', grade: 7 },] },
     nawodya: { password: '1234', subjects: [{ name: 'history', grade: 7 }] },
     osadee: { password: '770065988', subjects: [{ name: 'history', grade: 7 }] },
     dulmika: { password: 'Dq3%Vo7@', subjects: [{ name: 'history', grade: 7 }, { name: 'geography', grade: 7 },{ name: 'civic', grade: 7 }] },
