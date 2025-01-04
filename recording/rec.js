032d25c0136bd3965840cf2910a1392eb11e4fa7
samira <snukanimnal@gmail.com>
2025-01-04 19:46:38 +0530

    Update rec.js

--- recording/rec.js
+++ recording/rec.js
@@ -1,6 +1,33 @@
 const videoLinks = [
 
 
+      { 
+        class: '11', 
+        subject: 'History', 
+        date: '2025-01-03', 
+        month: 'January', 
+        video: 'https://drive.google.com/file/d/1xG7doG0h4_m_2vsQr-dS4iOPxqTaJvJZ/preview', 
+        title: '2025-01-03 - Class 11 - History Part 1' 
+      },
+
+      { 
+        class: '11', 
+        subject: 'History', 
+        date: '2025-01-03', 
+        month: 'January', 
+        video: 'https://drive.google.com/file/d/1zLvMZONMMTmNfHK1D-fiQn3AfoSWOiap/preview', 
+        title: '2025-01-03 - Class 11 - History Part 2' 
+      },
+
+      { 
+        class: '11', 
+        subject: 'History', 
+        date: '2025-01-03', 
+        month: 'January', 
+        video: 'https://drive.google.com/file/d/1am4SlPrrFdhYeCMxBjus9QchBI8kRAcs/preview', 
+        title: '2025-01-03 - Class 11 - History Part 3' 
+      },
+
 
   ];
 
