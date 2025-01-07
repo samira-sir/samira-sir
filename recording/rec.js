4d8bbb131dae84c0ed900902705a0bfa68d045dc
samira <snukanimnal@gmail.com>
2025-01-07 06:44:51 +0530

    Update rec.js

--- recording/rec.js
+++ recording/rec.js
@@ -38,6 +38,33 @@ const videoLinks = [
         title: '2025-01-02 - Class 9 - History Part 1' 
       },
 
+      { 
+        class: '11', 
+        subject: 'Geography', 
+        date: '2025-01-06', 
+        month: 'January', 
+        video: 'https://drive.google.com/file/d/1c_MALL3Q67Aly_mTvlNg31axdKDCdDpS/preview', 
+        title: '2025-01-06 - Class 11 - Geography Part 1' 
+      },
+
+      { 
+        class: '11', 
+        subject: 'Geography', 
+        date: '2025-01-06', 
+        month: 'January', 
+        video: 'https://drive.google.com/file/d/1Snuwdmkm3IebSKwxUvcdJxxE_asapMxA/preview', 
+        title: '2025-01-06 - Class 11 - Geography Part 2' 
+      },
+
+      { 
+        class: '11', 
+        subject: 'Geography', 
+        date: '2025-01-06', 
+        month: 'January', 
+        video: 'https://drive.google.com/file/d/1shf_23bHEBbnyqt4dDXLDWVYJOiZO5-y/preview', 
+        title: '2025-01-06 - Class 11 - Geography Part 3' 
+      },
+
   ];
 
   
