8bcf8b53002150f50d8013eb88ee87d2358842b4
samira <snukanimnal@gmail.com>
2025-02-06 20:03:48 +0530

    Update user.js

--- js/user.js
+++ js/user.js
@@ -6,6 +6,7 @@ const credentials = {
     nithuki: { password: '2014', subjects: [{ name: 'geography', grade: 6 }, { name: 'history', grade: 6 }] },
     shenya: { password: '2014', subjects: [{ name: 'geography', grade: 6 }, { name: 'history', grade: 6 }] },
     ihansa: { password: '2014', subjects: [{ name: 'geography', grade: 6 }, { name: 'history', grade: 6 }] },
+    lositha: { password: '2014', subjects: [{ name: 'geography', grade: 6 }, { name: 'history', grade: 6 }] },
     numaya: { password: 'abcde', subjects: [{ name: 'history', grade: 7 }] },
     kavitha: { password: 'kavitha123', subjects: [{ name: 'geography', grade: 7 }, { name: 'history', grade: 7 }] },
     imandi: { password: 'Gw7@Eh5%', subjects: [{ name: 'history', grade: 7 }, { name: 'geography', grade: 7 }] },
