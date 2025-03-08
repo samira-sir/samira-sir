8297ca7f97da0fa5195961ff418c7f072a18e261
samira <snukanimnal@gmail.com>
2025-03-08 23:08:19 +0530

    Add files via upload

---
+++ Control_Panel/pdf.js
@@ -0,0 +1,101 @@
+const pdfUrls = {
+    "6-history": [
+      "https://drive.google.com/file/d/1_CVezGJ_s0xN0Ag444bd6XtYERVdn5ak/view?usp=sharing",
+      "https://drive.google.com/file/d/1awrxFXvTE81nCj-l6gMKZaMcw5x3ZmXz/view?usp=sharing",
+    ],
+     "6-civic": [
+
+    ],
+    "6-geography": [
+      "https://drive.google.com/file/d/1cR--K3p9Xr-Po_aQLIw7W-iFx2wYpKd9/view?usp=sharing",
+      "https://drive.google.com/file/d/1LtXqy9SvzpZjEgvJWx3oG7supfVxtzpb/view?usp=sharing",
+    ],
+    "7-history": [
+      "https://drive.google.com/file/d/11_E_Mlq1PgM3no9n40RreYBu3cDBmFKE/view?usp=sharing",
+      "https://drive.google.com/file/d/14gkq9yA-zRYrfMJmO-r6cdVEcK1mlefU/view?usp=sharing",
+     "https://drive.google.com/file/d/13urskEr90a7iv3HajZwaNoNV8rruWisd/view?usp=sharing",
+     "https://drive.google.com/file/d/1ERN_IqBDegGe9VbcLMA4kcmT2cJ3Z1Df/view?usp=sharing",
+     "https://drive.google.com/file/d/1TXw9z-vKwesM2TiAGqP06cqbux_Feoo_/view?usp=sharing",
+     "https://drive.google.com/file/d/1QDSvrnAb6utzq3t5dV8lr_GWHTtOBill/view?usp=sharing",
+     
+    ],
+    "7-history": [
+      "https://drive.google.com/file/d/11_E_Mlq1PgM3no9n40RreYBu3cDBmFKE/view?usp=sharing",
+      "https://drive.google.com/file/d/14gkq9yA-zRYrfMJmO-r6cdVEcK1mlefU/view?usp=sharing",
+     "https://drive.google.com/file/d/13urskEr90a7iv3HajZwaNoNV8rruWisd/view?usp=sharing",
+     "https://drive.google.com/file/d/1ERN_IqBDegGe9VbcLMA4kcmT2cJ3Z1Df/view?usp=sharing",
+     "https://drive.google.com/file/d/1TXw9z-vKwesM2TiAGqP06cqbux_Feoo_/view?usp=sharing",
+     "https://drive.google.com/file/d/1QDSvrnAb6utzq3t5dV8lr_GWHTtOBill/view?usp=sharing",
+     
+    ],
+ "8-history": [
+      "https://drive.google.com/file/d/15GKODSr1VK4hVDGTTwfyVzouizMcC7xS/view?usp=sharing",
+      "https://drive.google.com/file/d/1DAkA2uX0jtnZhWybywC6MDJF7yJbrtbW/view?usp=sharing",
+     "https://drive.google.com/file/d/1Ho7RzXijnJFQ-4NpRScQlyR9R5o3Ux8o/view?usp=sharing",
+     
+    ],
+ "9-history": [
+      "https://drive.google.com/file/d/1s-DwDOtVxvYXAdFvh-SVZ9mgSWAeV0YQ/view?usp=sharing",
+      "https://drive.google.com/file/d/1GTr6NRLSsFNMT15SVojoGkra7bgzyXST/view?usp=sharing",
+     
+    ],
+"10-history": [
+     
+    ],
+ "7-geography": [
+      "https://drive.google.com/file/d/1KybfRHew0xBqBttmAMLP8Y3B5gPPto0Y/view?usp=sharing",
+     
+    ],
+ "8-geography": [
+      "https://drive.google.com/file/d/12og36_6SINi4KWQbGnZUw_y0jj0n0t-K/view?usp=sharing",
+      "https://drive.google.com/file/d/1TcIRX0iTsK-yKujpTIi-xS1RyJNietkP/view?usp=sharing",
+     "https://drive.google.com/file/d/1i5QiYCE7dfLy2npH3O0EkhTPXyzQje2v/view?usp=sharing",
+     "https://drive.google.com/file/d/1P0WErxSJrs2_NbUki8hBESLRYLCbljoC/view?usp=sharing",
+     "https://drive.google.com/file/d/14kTmIsN2K1TsUj6uCsNC5iwA1ggP9GKZ/view?usp=sharing",
+     
+    ],
+ "9-geography": [
+      "https://drive.google.com/file/d/1784AtdelT6g-pqpjZIuE8ht1Ys5pCdUR/view?usp=sharing",
+      "https://drive.google.com/file/d/1KN8S1idWWAiZW_HwhVISMux-ePGlHHgb/view?usp=sharing",
+     "https://drive.google.com/file/d/1F8ocixjl_NyUou4ytJtbWVM9IHYQkvYJ/view?usp=sharing",
+     "https://drive.google.com/file/d/1q8BgpuPbI8xtiCxh9K-bp3ueA2zcNH9L/view?usp=sharing",
+     "https://drive.google.com/file/d/12fyg-FAu4MCLOFY8m5-4NLEOnmlKwcdi/view?usp=sharing",
+    ],
+ "10-geography": [
+      "https://drive.google.com/file/d/1Aya568zxiSyQ2eRKoyPXmkTiMcWz9qOM/view?usp=sharing",
+      "https://drive.google.com/file/d/1FYVG7glMZWLdTvSN5YnkCbW37KBc9jar/view?usp=sharing",
+     "https://drive.google.com/file/d/1v8oCTM1xW5xig5-dLChY-pLnDLZ5VWXZ/view?usp=sharing",
+     "https://drive.google.com/file/d/1J4KA1ZxlFiq3ZxWVjhdLYwGrX6HrjOv0/view?usp=sharing",
+    ],
+ "7-civic": [
+      "https://drive.google.com/file/d/1az6BWcleE4p_tOW87jjhcyT9zmQ-S361/view?usp=sharing",
+      "https://drive.google.com/file/d/1oWKyHP6o8EeQUk07KKxA8NUU6ZLgPk2Z/view?usp=sharing",
+    ],
+ "8-civic": [
+      "https://drive.google.com/file/d/1KBVj0eX7IPFvYmDZxQpsC22TunfcMRo0/view?usp=sharing",
+      "https://drive.google.com/file/d/10emYc1uRCWq6JPSHex6VMyar7Dc1WKEg/view?usp=sharing",
+     "https://drive.google.com/file/d/1iqZSEVHKJDNepjUeuM11oGWn2Paewfel/view?usp=sharing",
+     "https://drive.google.com/file/d/16vvnOn6XhuSSi2QQfEqMDNAIslHp10Oc/view?usp=sharing",
+    ],
+ "9-civic": [
+      "https://drive.google.com/file/d/1omuBKEuFasx22HKedbLYZecXQJZK7O1p/view?usp=sharing",
+    ],
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
