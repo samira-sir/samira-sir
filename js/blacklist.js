8adf3e35597c7c767f4ee96c34c2535038766070
samira <snukanimnal@gmail.com>
2025-03-15 12:36:54 +0530

    Update blacklist.js

--- js/blacklist.js
+++ js/blacklist.js
@@ -1,1 +1,1 @@
-  const blacklist = ['dulya','himaka','eshini','nethuki','reema','oneli','nethini','vonadi','sansala','dewmi','lithumila']; 
+  const blacklist = ['dulya','himaka','eshini','nethuki','reema','oneli','nethini','vonadi','sansala','dewmi','lithumila','githuni','shakuni']; 
