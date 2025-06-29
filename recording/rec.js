5ea3d0ccf8f893e20cfcdf62f2095f74071c7d4f
samira <snukanimnal@gmail.com>
2025-06-29 06:41:26 +0530

    Update rec.js

--- recording/rec.js
+++ recording/rec.js
@@ -8057,6 +8057,33 @@ const videoLinks = [
       },
 
 
+      { 
+        class: '9', 
+        subject: 'Civic', 
+        date: '2025-06-27', 
+        month: 'June', 
+        video: 'https://drive.google.com/file/d/1CwxNwfYneDX-zran-fTxBtmUuFtkMhv-/preview', 
+        title: '2025-06-27 - Class 9 - Civic Part 1' 
+      },
+
+      { 
+        class: '9', 
+        subject: 'Civic', 
+        date: '2025-06-27', 
+        month: 'June', 
+        video: 'https://drive.google.com/file/d/1T1QecU99povUC5iFIfPccK1PM2FoivIB/preview', 
+        title: '2025-06-27 - Class 9 - Civic Part 2' 
+      },
+
+      { 
+        class: '9', 
+        subject: 'Civic', 
+        date: '2025-06-27', 
+        month: 'June', 
+        video: 'https://drive.google.com/file/d/1CSahhKZpUKVTXDKGJRXDA2OT2-ntWuG4/preview', 
+        title: '2025-06-27 - Class 9 - Civic Part 3' 
+      },
+
 
 
         ];
