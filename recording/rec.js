3883af5b0e1f0549176e10d9d6e773c026f49be0
samira <snukanimnal@gmail.com>
2025-08-02 21:38:27 +0530

    Update rec.js

--- recording/rec.js
+++ recording/rec.js
@@ -9616,5 +9616,33 @@ const videoLinks = [
         title: '2025-07-31 - Class 9 - History Part 4' 
       },
 
+      { 
+        class: '9', 
+        subject: 'Civic', 
+        date: '2025-08-01', 
+        month: 'August', 
+        video: 'https://drive.google.com/file/d/1CwxNwfYneDX-zran-fTxBtmUuFtkMhv-/preview', 
+        title: '2025-08-01 - Class 9 - Civic Part 1' 
+      },
+
+      { 
+        class: '9', 
+        subject: 'Civic', 
+        date: '2025-08-01', 
+        month: 'August', 
+        video: 'https://drive.google.com/file/d/1T1QecU99povUC5iFIfPccK1PM2FoivIB/preview', 
+        title: '2025-08-01 - Class 9 - Civic Part 2' 
+      },
+
+      { 
+        class: '9', 
+        subject: 'Civic', 
+        date: '2025-08-01', 
+        month: 'August', 
+        video: 'https://drive.google.com/file/d/1CSahhKZpUKVTXDKGJRXDA2OT2-ntWuG4/preview', 
+        title: '2025-08-01 - Class 9 - Civic Part 3' 
+      },
+
         ];
   
+
