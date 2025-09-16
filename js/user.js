df4ce4c8420d199d8ff8721f4fe48ef404255a18
samira <snukanimnal@gmail.com>
2025-09-16 16:56:07 +0530

    Update user.js

--- js/user.js
+++ js/user.js
@@ -71,7 +71,7 @@ const credentials = {
     dewmi: { password: 'TADAW', subjects: [{ name: 'history', grade: 9 }] },
     minketh: { password: 'Dq1%Ws6@', subjects: [{ name: 'geography', grade: 9 },{ name: 'civic', grade: 9}] },
     pinidi: { password: '2058', subjects: [{ name: 'history', grade: 9 },{ name: 'geography', grade: 9 }] }, 
-    nadun: { password: '1234', subjects: [{ name: 'geography', grade: 9 },
+    nadun: { password: '1234', subjects: [{ name: 'geography', grade: 9 }] }
     ishan: { password: '2010', subjects: [{ name: 'history', grade: 10 }] },
     udula: { password: 'udula123', subjects: [{ name: 'history', grade: 10 }] },
     praveen: { password: 'Nw8%Fu3@', subjects: [{ name: 'history', grade: 10 },{ name: 'geography', grade: 10 }] },
@@ -120,6 +120,7 @@ const credentials = {
    
 
   };
+
 
 
 
