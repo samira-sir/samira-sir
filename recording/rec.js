becf1313bf32ce584f4f16d83b33dbae10beced9
samira <snukanimnal@gmail.com>
2026-01-14 00:22:25 +0530

    Update rec.js

--- recording/rec.js
+++ recording/rec.js
@@ -10,9 +10,37 @@ const videoLinks = [
         title: '2025-01-03 - Class 11 - History Part 1' 
       },
 
+      { 
+        class: '6', 
+        subject: 'History', 
+        date: '2026-01-13', 
+        month: 'January', 
+        video: 'https://drive.google.com/file/d/1O_mvYz7-2XKzZp7PKQV1MlM3ARJs1L6P/preview', 
+        title: '2026-01-13 - Class 6 - History Part 1' 
+      },
+
+      { 
+        class: '6', 
+        subject: 'History', 
+        date: '2026-01-13', 
+        month: 'January', 
+        video: 'https://drive.google.com/file/d/1lyDeegdS2iyWR28NlOr0PdfGNLW4imdz/preview', 
+        title: '2026-01-13 - Class 6 - History Part 2' 
+      },
+
+      { 
+        class: '6', 
+        subject: 'History', 
+        date: '2026-01-13', 
+        month: 'January', 
+        video: 'https://drive.google.com/file/d/1Dh47sYtmJeacvmeyF-ZGWb8O0psskE6h/preview', 
+        title: '2026-01-13 - Class 6 - History Part 3' 
+      },
+
 
      ];
   
+
 
 
 
