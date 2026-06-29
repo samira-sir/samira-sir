b9e2ce895f38dfa53bf32aa2846a8a1faf87837d
samira <snukanimnal@gmail.com>
2026-06-29 10:37:50 +0530

    Update user.js

--- js/user.js
+++ js/user.js
@@ -99,6 +99,7 @@ const credentials = {
     niduka: { password: 'nidu1212', subjects: [{ name: 'history', grade: 10 }] },
     kenuli : { password: '1520', subjects: [{ name: 'geography', grade: 10 }] },
     vinudi : { password: '2011', subjects: [{ name: 'history', grade: 10 }] }, 
+    senumitha: { password: 'senumitha123', subjects: [{ name: 'history', grade: 10 },{ name: 'geography', grade: 10 },{ name: 'History Paper', grade: 10 }] },  
     ishan: { password: '2010', subjects: [{ name: 'history', grade: 11 },{ name: 'History Paper', grade: 11 }] },
     udula: { password: 'udula123', subjects: [{ name: 'history', grade: 11 },{ name: 'History Paper', grade: 11 }] },
     praveen: { password: 'Nw8%Fu3@', subjects: [{ name: 'history', grade: 11 },{ name: 'geography', grade: 11 },{ name: 'History Paper', grade: 11 }] },
