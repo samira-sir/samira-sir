38e23d779d29a8fe211476edc028aa526b26f012
samira <snukanimnal@gmail.com>
2026-08-02 21:45:10 +0530

    Update user.js

--- js/user.js
+++ js/user.js
@@ -132,6 +132,7 @@ const credentials = {
     thesanya : { password: 'Thesanya1002', subjects: [{ name: 'geography', grade: 11 }] }, 
     dishani: { password: '1980', subjects: [{name:'history',grade:11},{ name: 'History Paper', grade: 11 }] },
     nimuthu: { password: '200935', subjects: [{ name: 'history Paper', grade: 11 }] }, 
+    hasaru: { password: 'Has&1018', subjects: [{ name: 'history', grade: 11 }] },  
     };
 
 
