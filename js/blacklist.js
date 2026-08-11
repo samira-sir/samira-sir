4eff740ad34d3e4de07bb01f8049ddc244ef62d1
samira <snukanimnal@gmail.com>
2026-08-11 20:48:04 +0530

    Update blacklist.js

--- js/blacklist.js
+++ js/blacklist.js
@@ -1,1 +1,1 @@
-  const blacklist = ['himaka','eshini','nethuki','nethini','sansala','dewmi','lithumila','lositha','shakuni','minudi','adeesha','iduwara','saratha' ,'pinidi' ]; 
+  const blacklist = ['himaka','eshini','nethuki','nethini','sansala','dewmi','lithumila','lositha','shakuni','minudi','iduwara','saratha' ,'pinidi' ]; 
