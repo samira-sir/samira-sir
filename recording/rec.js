5b7431fdfe22789395780f9a5fe4bba9dde408b4
samira <snukanimnal@gmail.com>
2026-09-14 23:00:55 +0530

    Update rec.js

--- recording/rec.js
+++ recording/rec.js
@@ -10573,6 +10573,33 @@ const videoLinks = [
         title: '2026-09-13 - Grade 7 Geography  Class Part 3' 
       },
 
+      { 
+        class: '7', 
+        subject: 'History', 
+        date: '2026-09-13', 
+        month: 'September', 
+        video: 'https://drive.google.com/file/d/1lIdXzjTX6khSIoZJvYUZSr5ReHoecnZg/preview', 
+        title: '2026-09-13 - Grade 7 History  Class Part 1' 
+      },
+
+      { 
+        class: '7', 
+        subject: 'History', 
+        date: '2026-09-13', 
+        month: 'September', 
+        video: 'https://drive.google.com/file/d/1vQnKEwT3tnJzoZaaBarSpGAnooJu9Rh3/preview', 
+        title: '2026-09-13 - Grade 7 History  Class Part 2' 
+      },
+
+      { 
+        class: '7', 
+        subject: 'History', 
+        date: '2026-09-13', 
+        month: 'September', 
+        video: 'https://drive.google.com/file/d/1Q8-TR3AQsqAKujt4uxF1BxYRgwVPaI-e/preview', 
+        title: '2026-09-13 - Grade 7 History  Class Part 3' 
+      },
+
      ];
   
 
